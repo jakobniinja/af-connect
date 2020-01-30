82a82a3e60cfaed835dc31eb59815d1329ef2a36
Anton Wiklund <anton.wiklund@arbetsformedlingen.se>
2020-01-30 11:52:46 +0100

    Removed unused code

--- app/lib/logger/index.js
+++ app/lib/logger/index.js
@@ -1,37 +1,19 @@
-'use strict';
+"use strict";
 
-const { createLogger, format, transports } = require('winston');
-const fs = require('fs');
-const path = require('path');
-
-/** 
-const env = process.env.NODE_ENV || 'development';
-const logDir = `../../../logs`;
-
-if (!fs.existsSync(logDir)) {
-    fs.mkdirSync(logDir);
-}
-*/
-
-// const filename = path.join(logDir, 'main.log');
-
-// TODO: Nedd to add logfile 
+const { createLogger, format, transports } = require("winston");
 
 const logger = createLogger({
-    level: 'debug',
-    exitOnError: false,
-    format: format.combine(
-        format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss'
-        }),
-        format.json()
-    ),
-    transports: [
-        new transports.Console()
-    ]
+  level: "debug",
+  exitOnError: false,
+  format: format.combine(
+    format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss"
+    }),
+    format.json()
+  ),
+  transports: [new transports.Console()]
 });
 
-logger.info('Hello world');
-logger.debug('Debugging info');
+logger.info("Logger initalized");
 
 module.exports = logger;
